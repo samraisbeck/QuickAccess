99a44324989241c4cd2149325fcc1a558c791355
Sam Raisbeck <ssraisbeck10@gmail.com>
2017-07-31 16:45:21 -0400

    First commit with code
    There will be another commit right after adding config.yaml to the gitignore. I just wanted to push a blank version of the file so people can pull it and add their own stuff, but then it won't try to push that back to the remote.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # QuickAccess
 Convenient program that makes launching personal programs super quick.
+### How to Use
+Using this is very easy. If you have a program that can be launched from the command line,
+it can be added to QuickAccess! All you must do is open up `config.yaml` and add the
+entry you would like. For example, if you have a program called "Hey There", and it is
+usually run by going to your documents and running `hey_there.py`, just do
+`Hey There: C:\Users\username\Documents\hey_there.py` in the yaml file. This will tell
+QuickAccess to add a button to the GUI titled "Hey There", and when clicked, run `hey_there.py` in
+the documents folder.
+
+Sometimes, I get frustrated when I accidently close a command prompt when I want to restart a program,
+or having to open multiple command prompt tabs or windows to run different programs. That's why I
+made this simple program, so that all of the programs I want to run are laid out in front of me as
+buttons that, when clicked, runs the program right away.
+
+If you think of anything else to add, feel free to contribute.
+
+I will probably make it look nicer in the future, this is just a small idea I had.
+I might make it so that you can add programs to QuickAccess via the GUI rather than having
+to manually add them into the yaml file. This obviously would make it much easier to use for
+people who aren't familiar with configuration files and stuff like that.
---
+++ main.py
@@ -0,0 +1,60 @@
+from PySide import QtCore, QtGui
+import yaml
+from subprocess import *
+import os, sys
+
+class ProgramManager(QtGui.QMainWindow):
+    def __init__(self):
+        super(ProgramManager, self).__init__()
+        self.programs = None
+        self.keys = []
+        self.readConfig()
+        self._initUI()
+
+    def readConfig(self):
+        try:
+            with open('config.yaml', 'r') as f:
+                try:
+                    self.programs = yaml.safe_load(f)
+                except yaml.YAMLError:
+                    print 'Could not read config.yaml.'
+                    raise
+        except IOError:
+            print 'Could not find the config.yaml file.'
+            raise
+        if self.programs is not None:
+            self.keys = self.programs.keys()
+
+
+    def _initUI(self):
+        if self.programs == None:
+            print 'No programs configured. Exiting...'
+            sys.exit(0)
+        label = QtGui.QLabel('QuickAccess for Windows', parent=self)
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setPointSize(15)
+        label.setFont(font)
+        grid = QtGui.QGridLayout()
+        grid.addWidget(label, 0, 0, alignment=QtCore.Qt.AlignHCenter)
+        buttonGrid = QtGui.QGridLayout()
+        for i in range(len(self.programs)):
+            button = QtGui.QPushButton(self.keys[i], parent=self)
+            self.connect(button, QtCore.SIGNAL('pressed()'), self.launchProgram)
+            buttonGrid.addWidget(button, 0, i)
+        grid.addLayout(buttonGrid, 1, 0)
+        Qw = QtGui.QWidget()
+        Qw.setLayout(grid)
+        self.setCentralWidget(Qw)
+        self.setWindowTitle('QuickAccess for '+os.environ['USERNAME']+' on '+os.environ['COMPUTERNAME'])
+        self.show()
+
+    def launchProgram(self):
+        button = self.sender()
+        text = button.text()
+        console = Popen([sys.executable, self.programs[text]], cwd = os.path.dirname(self.programs[text]), creationflags=CREATE_NEW_CONSOLE)
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    mw = ProgramManager()
+    app.exec_()
